fea1f58272a5192e5be40a09d984680c5fee0df4
Nathan Pham <nathanpham.me@gmail.com>
2021-06-21 21:13:49 -0700

    modernize

--- js/index.js
+++ js/index.js
@@ -1,65 +1,68 @@
-import define, { jsh } from "https://unpkg.com/stateful-components@2.0.7/dist/index.js"
-const { a, img, div } = jsh
+import define, { jsh as h } from "https://unpkg.com/stateful-components@2.1.0/dist/index.js"
+
+const images = {
+    github: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+    replit: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Repl.it_logo.svg/1200px-Repl.it_logo.svg.png"
+}
 
 define("w-badge-container", {
-    style() {
-        return `
-            div {
-                position: fixed;
-                bottom: 1rem;
-                right: 1rem;
-                display: flex;
-                flex-direction: column;
-            }
-        `
-    },
-    render(state, props) {
-        return div({}, ...props.childNodes)
+    style: `
+        div {
+            position: fixed;
+            bottom: 1rem;
+            right: 1rem;
+            display: flex;
+            flex-direction: column;
+        }
+    `,
+
+    render(_, target) {
+        return h.div({}, ...target.childNodes)
     }
 })
 
 define("w-badge", {
-    style() {
-        return `
-            a {
-                padding: 0.75rem 1rem;
-                background: #000;
-                margin: 1rem 0 0 0;
-                color: #fff;
-                text-decoration: none;
-                display: flex;
-                align-items: center;
-                transition: box-shadow 0.2s ease;
-            }
+    state: {props: {}},
 
-            a:hover {
-                box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.25);
-            }
-
-            a img {
-                background: #fff;
-                border-radius: 50%;
-                max-height: 1.5rem;
-                margin: 0 0.75rem 0 0;
-                object-fit: cover;
-            }
-        `
-    },
-    render(state, props) {
-        const images = {
-            github: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-            replit: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Repl.it_logo.svg/1200px-Repl.it_logo.svg.png"
+    style: `
+        a {
+            padding: 0.75rem 1rem;
+            background: #000;
+            margin: 1rem 0 0 0;
+            color: #fff;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            transition: box-shadow 0.2s ease;
         }
 
-        const type = (props.getAttribute("type") || "github").toLowerCase()
-        const tag = props.getAttribute("tag") || "nathan-pham"
-        const href = `https://${type}.com/${tag}`
-        const text = props.textContent || type
-        const src = props.getAttribute("src") || images[type]
+        a:hover {
+            box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.25);
+        }
 
+        a img {
+            background: #fff;
+            border-radius: 50%;
+            max-height: 1.5rem;
+            margin: 0 0.75rem 0 0;
+            object-fit: cover;
+        }
+    `,
+    
+    render(state, target) {
+        const props = {}
+        for(const key of target.getAttributeNames()) {
+            props[key] = target.getAttribute(key)
+        }
+
+        const {type="github", tag="nathan-pham", src} = props
+
+        const href = `https://${type}.com/${tag}`
+        const text = target.textContent || type
+        
         return (
-            a({ href, target: "_blank", rel: "noreferrer" },
-                img({ src, alt: `${type} logo` }),
+            h.a({ href, target: "_blank", rel: "noreferrer" },
+                h.img({ src: src || images[type], alt: `${type} logo` }),
                 text
             )
         )
